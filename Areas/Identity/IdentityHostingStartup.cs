9377b184d0236fab944681641882af4a674dae99
hillj0 <joel.hill@lightsource.ca>
2020-08-19 08:13:32 -0600

    Adding Schema for identity

---
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,27 @@
+using System;
+using En_Garde.Areas.Identity.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(En_Garde.Areas.Identity.IdentityHostingStartup))]
+namespace En_Garde.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<en_gardeIdentityDbContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("en_gardeIdentityDbContextConnection")));
+
+                services.AddDefaultIdentity<Player>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<en_gardeIdentityDbContext>();
+            });
+        }
+    }
+}
